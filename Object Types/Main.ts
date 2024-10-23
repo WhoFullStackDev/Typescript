bcbf4eeabd0d01a179f392533d9b62c5d3e27af7
Kingsuk <work.kingsuk@gmail.com>
2024-10-23 16:40:14 +0530

    Add some new learning

--- Object Types/Main.ts
+++ Object Types/Main.ts
@@ -1,4 +1,4 @@
-// Optional Properties
+// ********************Optional Properties********************
 interface PaintOptions {
   shape: Shape;
   xPos?: number;
@@ -16,7 +16,7 @@ paintShape({ shape, xPos: 100 });
 paintShape({ shape, yPos: 100 });
 paintShape({ shape, xPos: 100, yPos: 100 });
 
-// readonly Properties
+// ********************readonly Properties********************
 interface someType {
   readonly props: string;
 }
@@ -69,7 +69,7 @@ console.log(readonlyPerson.age); // prints '42'
 writablePerson.age++;
 console.log(readonlyPerson.age); // prints '43'
 
-// Index Signatures
+// ********************Index Signatures********************
 interface StringArray {
   [index: number]: string;
 }
@@ -96,7 +96,7 @@ interface ReadonlyStringArray {
 let myArray2: ReadonlyStringArray = getReadOnlyStringArray();
 myArray2[2] = "Mallory";
 
-//Excess Property Checks
+//********************Excess Property Checks********************
 interface SquareConfig {
   color?: string;
   width?: number;
@@ -121,7 +121,7 @@ interface SquareConfig {
 let squareOptions = { colour: "red", width: 100 };
 mySquare = createSquare(squareOptions);
 
-// Extending Type
+// ********************Extending Type********************
 // interface BasicAddress {
 //   name?: string;
 //   street: string;
@@ -166,7 +166,7 @@ const cc: ColorfulCircle = {
   radius: 42,
 };
 
-//Intersection Types
+//********************Intersection Types********************
 interface Colorful {
   color: string;
 }
@@ -184,3 +184,167 @@ draw({ color: "blue", radius: 42 });
 
 // oops
 draw({ color: "red", raidus: 42 });
+
+// ********************Generic Object Types********************
+interface Box<Type> {
+  contents: Type;
+}
+
+interface StringBox {
+  contents: string;
+}
+
+let boxA: Box<string> = { contents: "hello" };
+boxA.contents;
+
+let boxB: StringBox = { contents: "world" };
+boxB.contents;
+
+interface Apple {
+  // ....
+}
+
+// Same as '{ contents: Apple }'.
+type AppleBox = Box<Apple>;
+
+function setContents<Type>(box: Box<Type>, newContents: Type) {
+  box.contents = newContents;
+}
+
+type Boxes<Type> = {
+  contents: Type;
+};
+
+type OrNull<Type> = Type | null;
+
+type OneOrMany<Type> = Type | Type[];
+
+type OneOrManyOrNull<Type> = OrNull<OneOrMany<Type>>;
+
+type OneOrManyOrNullStrings = OneOrManyOrNull<string>;
+
+// ********************The Array Type********************
+function doSomethings(value: Array<string>) {
+  // ...
+}
+
+let myArrays: string[] = ["hello", "world"];
+
+// either of these work!
+doSomethings(myArrays);
+doSomethings(new Array("hello", "world"));
+
+interface Array<Type> {
+  /**
+   * Gets or sets the length of the array.
+   */
+  length: number;
+
+  /**
+   * Removes the last element from an array and returns it.
+   */
+  pop(): Type | undefined;
+
+  /**
+   * Appends new elements to an array, and returns the new length of the array.
+   */
+  push(...items: Type[]): number;
+
+  // ...
+}
+
+// The ReadonlyArray Type
+function doStuff(values: ReadonlyArray<string>) {
+  // We can read from 'values'...
+  const copy = values.slice();
+  console.log(`The first value is ${values[0]}`);
+
+  // ...but we can't mutate 'values'.
+  values.push("hello!");
+}
+
+new ReadonlyArray("red", "green", "blue");
+
+const roArray: ReadonlyArray<string> = ["red", "green", "blue"];
+
+function doStuffs(values: readonly string[]) {
+  // We can read from 'values'...
+  const copy = values.slice();
+  console.log(`The first value is ${values[0]}`);
+
+  // ...but we can't mutate 'values'.
+  values.push("hello!");
+}
+
+let x: readonly string[] = [];
+let y: string[] = [];
+
+x = y;
+y = x;
+
+// *********************Tuple Types*******************
+type StringNumberPair = [string, number];
+
+function doSomethinges(pair: [string, number]) {
+  const a = pair[0];
+
+  const b = pair[1];
+  // ...
+  const c = pair[2];
+}
+
+doSomethinges(["hello", 42]);
+
+function doSomething2(stringHash: [string, number]) {
+  const [inputString, hash] = stringHash;
+
+  console.log(inputString);
+
+  console.log(hash);
+}
+
+interface StringNumberPair1 {
+  // specialized properties
+  length: 2;
+  0: string;
+  1: number;
+
+  // Other 'Array<string | number>' members...
+  slice(start?: number, end?: number): Array<string | number>;
+}
+
+type Either2dOr3d = [number, number, number?];
+
+function setCoordinate(coord: Either2dOr3d) {
+  const [x, y, z] = coord;
+
+  console.log(`Provided coordinates had ${coord.length} dimensions`);
+}
+
+type StringNumberBooleans = [string, number, ...boolean[]];
+type StringBooleansNumber = [string, ...boolean[], number];
+type BooleansStringNumber = [...boolean[], string, number];
+const a: StringNumberBooleans = ["hello", 1];
+const b: StringNumberBooleans = ["beautiful", 2, true];
+const c: StringNumberBooleans = ["world", 3, true, false, true, false, true];
+
+function readButtonInput(...args: [string, number, ...boolean[]]) {
+  const [name, version, ...input] = args;
+  // ...
+}
+
+function readButtonInput(name: string, version: number, ...input: boolean[]) {
+  // ...
+}
+
+function doSomething3(pair: readonly [string, number]) {
+  pair[0] = "hello!";
+}
+
+let point = [3, 4] as const;
+
+function distanceFromOrigin([x, y]: [number, number]) {
+  return Math.sqrt(x ** 2 + y ** 2);
+}
+
+distanceFromOrigin(point);
